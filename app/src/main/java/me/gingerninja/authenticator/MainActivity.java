eb45bf1ecc72fafed51720c82f03d1fd5b01ee65
Gergely Kőrössy <korossyg@gmail.com>
2018-09-17 15:54:28 +0200

    Added some navigation related code

--- app/src/main/java/me/gingerninja/authenticator/MainActivity.java
+++ app/src/main/java/me/gingerninja/authenticator/MainActivity.java
@@ -5,10 +5,13 @@ import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 import dagger.android.AndroidInjection;
 import me.gingerninja.authenticator.data.db.entity.Account;
+import me.gingerninja.authenticator.databinding.ActivityMainBinding;
 import me.gingerninja.authenticator.util.CodeGenerator;
 import me.gingerninja.authenticator.util.Parser;
 import me.gingerninja.authenticator.util.TimeCorrector;
@@ -17,13 +20,19 @@ public class MainActivity extends AppCompatActivity {
 
     private NavController navController;
 
+    private DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         AndroidInjection.inject(this);
         super.onCreate(savedInstanceState);
-        DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        drawerLayout = binding.drawerLayout;
 
         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+
+        NavigationUI.setupWithNavController(binding.navigationView, navController);
 
         Account account = Parser.parseUrl("otpauth://totp/ACME%20Co:john@example.com?secret=HXDMVJECJJWSRB3HWIZR4IFUGFTMXBOZ&issuer=ACME%20Co&algorithm=SHA1&digits=6&period=30");
         TimeCorrector timeCorrector = new TimeCorrector();
@@ -35,6 +44,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onSupportNavigateUp() {
-        return navController.navigateUp();
+        return NavigationUI.navigateUp(drawerLayout, navController);
     }
 }
