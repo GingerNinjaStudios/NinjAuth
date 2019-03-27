2ceefa2a0726c0ce2cd5a66bb3f27420ff55cf20
Gergely Kőrössy <korossyg@gmail.com>
2019-03-27 20:13:54 +0100

    RoundedBottomSheetDialogFragment uses MaterialShapeDrawable for corners instead of GradientDrawable

--- app/src/main/java/me/gingerninja/authenticator/ui/base/RoundedBottomSheetDialogFragment.java
+++ app/src/main/java/me/gingerninja/authenticator/ui/base/RoundedBottomSheetDialogFragment.java
@@ -1,21 +1,25 @@
 package me.gingerninja.authenticator.ui.base;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.InsetDrawable;
 import android.os.Build;
 import android.util.TypedValue;
 import android.view.View;
 
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.google.android.material.shape.MaterialShapeDrawable;
+import com.google.android.material.shape.RoundedCornerTreatment;
+import com.google.android.material.shape.ShapeAppearanceModel;
 
 import me.gingerninja.authenticator.R;
 
 public class RoundedBottomSheetDialogFragment extends BottomSheetDialogFragment {
-    private static Drawable createBackground(Context context) {
+    protected MaterialShapeDrawable shapeDrawable;
+
+    private Drawable createBackground(Context context) {
         TypedValue typedValue = new TypedValue();
         Resources.Theme theme = context.getTheme();
         theme.resolveAttribute(R.attr.colorSurface, typedValue, true);
@@ -31,17 +35,42 @@ public class RoundedBottomSheetDialogFragment extends BottomSheetDialogFragment 
 
         arr.recycle();
 
-        int cornerRadius = context.getResources().getDimensionPixelSize(R.dimen.bottom_sheet_corner_radius);
+        /*int cornerRadius = context.getResources().getDimensionPixelSize(R.dimen.bottom_sheet_corner_radius);
         int topPadding = context.getResources().getDimensionPixelSize(R.dimen.bottom_sheet_top_padding);
 
         GradientDrawable shapeDrawable = new GradientDrawable();
         shapeDrawable.setCornerRadii(new float[]{cornerRadius, cornerRadius, cornerRadius, cornerRadius, 0, 0, 0, 0});
-        shapeDrawable.setColor(color);
+        shapeDrawable.setColor(color);*/
 
-        return new InsetDrawable(shapeDrawable, 0, topPadding, 0, 0);
+        shapeDrawable = new MaterialShapeDrawable();
+        shapeDrawable.setFillColor(ColorStateList.valueOf(color));
+
+        ShapeAppearanceModel shapeModel = shapeDrawable.getShapeAppearanceModel();
+        shapeModel.setBottomLeftCorner(new NoCornerTreatment());
+        shapeModel.setBottomRightCorner(new NoCornerTreatment());
+
+        shapeDrawable.setCornerRadius(context.getResources().getDimensionPixelSize(R.dimen.bottom_sheet_corner_radius));
+
+        return shapeDrawable;
+
+        //return new InsetDrawable(shapeDrawable, 0, topPadding, 0, 0);
     }
 
     protected void createRoundedView(View view) {
         view.setBackground(createBackground(view.getContext()));
+    }
+
+    private static class NoCornerTreatment extends RoundedCornerTreatment {
+        /**
+         * Instantiates a no-corner treatment.
+         */
+        private NoCornerTreatment() {
+            super(0);
+        }
+
+        @Override
+        public void setCornerSize(float cornerSize) {
+            // do nothing, the corner size is always 0
+        }
     }
 }
