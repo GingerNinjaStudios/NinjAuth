2d2c35b795be8b78b984cf0e8bf909d2cfc751ad
Gergely Kőrössy <korossyg@gmail.com>
2019-05-15 14:12:25 +0200

    Dependency upgrades; bottom nav fragment fixed for new material-components; DatabaseHandler temp fix for requery 1.6.0

--- app/src/main/java/me/gingerninja/authenticator/data/db/provider/DatabaseHandler.java
+++ app/src/main/java/me/gingerninja/authenticator/data/db/provider/DatabaseHandler.java
@@ -2,8 +2,6 @@ package me.gingerninja.authenticator.data.db.provider;
 
 import android.content.Context;
 
-import net.sqlcipher.database.SQLiteDatabase;
-
 import io.requery.Persistable;
 import io.requery.android.sqlcipher.SqlCipherDatabaseSource;
 import io.requery.reactivex.ReactiveEntityStore;
@@ -11,10 +9,11 @@ import io.requery.reactivex.ReactiveSupport;
 import io.requery.sql.Configuration;
 import io.requery.sql.ConfigurationBuilder;
 import io.requery.sql.EntityDataStore;
+import io.requery.sql.StatementListener;
 import io.requery.sql.TableCreationMode;
 import me.gingerninja.authenticator.BuildConfig;
 import me.gingerninja.authenticator.data.db.DbConstants;
-import me.gingerninja.authenticator.data.db.entity.Models;
+import me.gingerninja.authenticator.data.db.Models;
 
 public class DatabaseHandler {
     private final Context context;
@@ -33,6 +32,8 @@ public class DatabaseHandler {
 
         ReactiveEntityStore<Persistable> store;
         // override onUpgrade to handle migrating to a new version
+        SqlCipherDatabaseSource source = new SqlCipherDatabaseSource(context, Models.DEFAULT, DbConstants.NAME, password, DbConstants.VERSION);
+        /* FIXME commented out because it's final in v1.6.0
         SqlCipherDatabaseSource source = new SqlCipherDatabaseSource(context, Models.DEFAULT, DbConstants.NAME, password, DbConstants.VERSION) {
             @Override
             public void onOpen(SQLiteDatabase db) {
@@ -47,15 +48,28 @@ public class DatabaseHandler {
                 builder.setQuoteColumnNames(true);
                 builder.setQuoteTableNames(true);
             }
-        };
+        };*/
 
         if (BuildConfig.DEBUG) {
             // use this in development mode to drop and recreate the tables on every upgrade
             source.setTableCreationMode(TableCreationMode.DROP_CREATE);
+
         }
 
-        Configuration configuration = source.getConfiguration();
-        store = ReactiveSupport.toReactiveStore(new EntityDataStore<>(configuration));
+        Configuration origConfig = source.getConfiguration();
+        ConfigurationBuilder newConfigBuilder = new ConfigurationBuilder(origConfig.getConnectionProvider(), origConfig.getModel())
+                .setMapping(origConfig.getMapping())
+                .setPlatform(origConfig.getPlatform())
+                .setQuoteColumnNames(true)
+                .setQuoteTableNames(true)
+                .setBatchUpdateSize(origConfig.getBatchUpdateSize());
+
+        for (StatementListener listener : origConfig.getStatementListeners()) {
+            newConfigBuilder.addStatementListener(listener);
+        }
+
+
+        store = ReactiveSupport.toReactiveStore(new EntityDataStore<>(newConfigBuilder.build()));
 
         this.databaseSource = source;
         this.entityStore = store;
