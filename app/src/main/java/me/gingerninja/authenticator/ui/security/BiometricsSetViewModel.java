b181c31b0c9da817040644203b6fda35958bbc11
Gergely Kőrössy <korossyg@gmail.com>
2019-07-02 23:59:20 +0200

    Added error handling to biometrics

--- app/src/main/java/me/gingerninja/authenticator/ui/security/BiometricsSetViewModel.java
+++ app/src/main/java/me/gingerninja/authenticator/ui/security/BiometricsSetViewModel.java
@@ -3,7 +3,10 @@ package me.gingerninja.authenticator.ui.security;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.biometric.BiometricConstants;
 import androidx.databinding.ObservableBoolean;
+import androidx.databinding.ObservableField;
+import androidx.databinding.ObservableInt;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
@@ -13,6 +16,8 @@ import javax.inject.Inject;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import me.gingerninja.authenticator.R;
+import me.gingerninja.authenticator.crypto.BiometricException;
 import me.gingerninja.authenticator.crypto.Crypto;
 import me.gingerninja.authenticator.util.SingleEvent;
 import timber.log.Timber;
@@ -28,6 +33,9 @@ public class BiometricsSetViewModel extends ViewModel {
 
     static final String EVENT_SKIP = "event.skip";
 
+    static final String EVENT_BIO_ERROR_ENROLL = "event.error.enroll";
+    static final String EVENT_BIO_ERROR_UNLOCK = "event.error.unlock";
+
     @NonNull
     private final Crypto crypto;
 
@@ -35,13 +43,18 @@ public class BiometricsSetViewModel extends ViewModel {
 
     private MutableLiveData<SingleEvent> events = new MutableLiveData<>();
 
-    public boolean isBioEnabled;
+    public ObservableBoolean isBioEnabled = new ObservableBoolean(false);
+    public ObservableInt bioErrorText = new ObservableInt(0);
+    public ObservableBoolean bioErrorBtnVisible = new ObservableBoolean(false);
+    public ObservableInt bioErrorBtnText = new ObservableInt(0);
+    public ObservableField<View.OnClickListener> bioErrorBtnClick = new ObservableField<>(null);
+
     public ObservableBoolean inputEnabled = new ObservableBoolean(true);
 
     @Inject
     BiometricsSetViewModel(@NonNull Crypto crypto) {
         this.crypto = crypto;
-        isBioEnabled = crypto.isBioEnabled();
+        isBioEnabled.set(crypto.isBioEnabled());
     }
 
     @Override
@@ -53,7 +66,7 @@ public class BiometricsSetViewModel extends ViewModel {
     public void onAuthClick(View v) {
         inputEnabled.set(false);
 
-        if (isBioEnabled) {
+        if (isBioEnabled.get()) {
             events.setValue(new SingleEvent(EVENT_DISABLE));
         } else {
             events.setValue(new SingleEvent(EVENT_ENABLE));
@@ -64,12 +77,26 @@ public class BiometricsSetViewModel extends ViewModel {
         events.setValue(new SingleEvent(EVENT_SKIP));
     }
 
+    private void onBioErrorBtnClickForEnrollment(View v) {
+        events.setValue(new SingleEvent(EVENT_BIO_ERROR_ENROLL));
+    }
+
+    private void onBioErrorBtnClickForUnlock(View v) {
+        events.setValue(new SingleEvent(EVENT_BIO_ERROR_UNLOCK));
+    }
+
     public LiveData<SingleEvent> getEvents() {
         return events;
     }
 
     void createBiometrics(Fragment fragment, char[] password) {
         disposable.clear();
+
+        inputEnabled.set(false);
+        bioErrorBtnVisible.set(false);
+        bioErrorBtnText.set(0);
+        bioErrorBtnClick.set(null);
+        bioErrorText.set(0);
 
         disposable.add(
                 crypto.create(fragment, password)
@@ -78,6 +105,40 @@ public class BiometricsSetViewModel extends ViewModel {
                                     events.setValue(new SingleEvent(EVENT_ENABLE_SUCCESS));
                                 },
                                 throwable -> {
+                                    if (throwable instanceof BiometricException) {
+                                        int errCode = ((BiometricException) throwable).getErrorCode();
+                                        switch (errCode) {
+                                            case BiometricException.ERROR_KEY_INVALIDATED:
+                                                bioErrorText.set(R.string.biometric_error_key_invalidated);
+                                                break;
+                                            case BiometricConstants.ERROR_LOCKOUT:
+                                                bioErrorText.set(R.string.biometric_error_temporary_lock);
+                                                break;
+                                            case BiometricConstants.ERROR_LOCKOUT_PERMANENT:
+                                                bioErrorText.set(R.string.biometric_error_permanent_lock);
+                                                bioErrorBtnVisible.set(true);
+                                                bioErrorBtnText.set(R.string.biometric_error_permanent_lock_btn);
+                                                bioErrorBtnClick.set(this::onBioErrorBtnClickForEnrollment);
+                                                break;
+                                            case BiometricConstants.ERROR_NO_BIOMETRICS:
+                                                bioErrorText.set(R.string.biometric_error_no_biometrics);
+                                                bioErrorBtnVisible.set(true);
+                                                bioErrorBtnText.set(R.string.biometric_error_no_biometrics_btn);
+                                                bioErrorBtnClick.set(this::onBioErrorBtnClickForEnrollment);
+                                                break;
+                                            case BiometricConstants.ERROR_TIMEOUT:
+                                            case BiometricConstants.ERROR_NEGATIVE_BUTTON:
+                                            case BiometricConstants.ERROR_CANCELED:
+                                            case BiometricConstants.ERROR_USER_CANCELED:
+                                                bioErrorText.set(0);
+                                                break;
+                                            default:
+                                                bioErrorText.set(R.string.biometric_error_unknown);
+                                        }
+                                    } else {
+                                        bioErrorText.set(R.string.biometric_error_unknown);
+                                    }
+
                                     inputEnabled.set(true);
                                     Timber.v(throwable, "Bio error");
                                     events.setValue(new SingleEvent(EVENT_ENABLE_FAIL, throwable));
